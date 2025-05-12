ebfc5af5e9ab4fa004320d7c0c3844cb119cb636
Gustavo <slima.gusta@gmail.com>
2025-05-12 20:35:32 -0300

    Criando as controllers Produto e Usuario

---
+++ ProjetoEcommerce2/Controllers/ProdutoController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoEcommerce2.Models;
+using ProjetoEcommerce2.Repositorio;
+
+namespace ProjetoEcommerce2.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly ProdutoRepositorio _produtoRespositorio;
+
+        public ProdutoController(ProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRespositorio = produtoRepositorio;
+        }
+        public ActionResult Index()
+        {
+            return View(_produtoRespositorio.TodosProdutos());
+        }
+
+        public IActionResult CadastrarProduto()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CadastrarProduto(Produto produto)
+        {
+
+            _produtoRespositorio.Cadastrar(produto);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult EditarProduto(int id)
+        {
+            var produto = _produtoRespositorio.ObterProduto(id);
+
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return View(produto);
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditarProduto(int id, [Bind("IdProd, Nome, Descricao, Quantidade, Preco")] Produto produto)
+        {
+            if
+                return BadRequest();
+        }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (_produtoRespositorio.Atualizar(produto))
+                    {
+                        return RedirectToAction(nameof(Index));
+    }
+}
+                catch (Exception)
+                {
+    ModelState.AddModelError("", "Ocorreu um erro ao Editar.");
+    return View(produto);
+}
+            }
+            return View(produto);
+        }
+
+        public IActionResult ExcluirProduto(int id)
+{
+    _produtoRespositorio.Excluir(id);
+    return RedirectToAction(nameof(Index));
+}
+
+    }
+}
---
+++ ProjetoEcommerce2/Controllers/UsuarioController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoEcommerce2.Repositorio;
+
+namespace ProjetoEcommerce2.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly UsuarioRepositorio _usuarioRepositorio;
+
+        public UsuarioController(UsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
+
+            var usuario = _usuarioRepositorio.ObterUsuario(email);
+            if (usuario != null && usuario.Senha == senha)
+            {
+                return RedirectToAction("Index", "Cliente");
+            }
+
+            ModelState.AddModelError("", "Email ou senha inválidos.");
+            return View();
+        }
+    }
+}
